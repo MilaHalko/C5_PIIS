51a3aa37080afeea2e0ebc082206c304fe76a03b
milka_deluxe <milagalko1@gmail.com>
2022-09-15 12:33:19 +0300

    Initial Commit

---
+++ Lab1_PIIS_A*_LI/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab1_PIIS_A*_LI
+//
+//  Created by Mila on 15.09.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
