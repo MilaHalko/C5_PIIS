dfa2eaf14380ed0c8979783a1af6a4b1b2c616c2
milka_deluxe <milagalko1@gmail.com>
2022-10-10 19:43:12 +0300

    create: minimax full algorithm todo: cell class

---
+++ Pacman/Pacman/Algos/MiniMax.cs
@@ -0,0 +1,45 @@
+using Pacman.Classes;
+
+namespace Pacman.Algos;
+
+public class MiniMax
+{
+    private Field _field;
+    // TODO: What is depth???
+    private int _depth = 6;
+
+    public MiniMax(Field f)
+    {
+        _field = f;
+    }
+
+    public int Apply(Cell position, int depth, bool maxPlayer)
+    {
+        if (depth == 0 || _field.EnemyWon())
+        {
+            return position.GetScore();
+        }
+
+        int bestScore = maxPlayer? Int32.MinValue : Int32.MaxValue;
+        foreach (var adjacent in position.GetAdjCells()) 
+        {
+            int score = Apply(adjacent, depth - 1, !maxPlayer);
+            bestScore = maxPlayer? Math.Max(bestScore, score) : Math.Min(bestScore, score);
+        }
+        return bestScore;
+    }
+}
+
+public class Cell
+{
+    public int GetScore()
+    {
+        return 1;
+    }
+
+    // TODO: Get adjacent cells?
+    public Cell[] GetAdjCells()
+    {
+        return new Cell();
+    }
+}
