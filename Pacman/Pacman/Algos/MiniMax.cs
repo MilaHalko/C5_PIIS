4c2d4623f44b58ac37813116f39436c8e2ac34be
milka_deluxe <milagalko1@gmail.com>
2022-10-10 20:02:24 +0300

    fix: delete cell class from Minimax class

--- Pacman/Pacman/Algos/MiniMax.cs
+++ Pacman/Pacman/Algos/MiniMax.cs
@@ -29,17 +29,3 @@ public class MiniMax
         return bestScore;
     }
 }
-
-public class Cell
-{
-    public int GetScore()
-    {
-        return 1;
-    }
-
-    // TODO: Get adjacent cells?
-    public Cell[] GetAdjCells()
-    {
-        return new Cell();
-    }
-}
