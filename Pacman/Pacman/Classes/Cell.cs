5fca6b77134306de5b0e1304d3839f17b7ba049d
milka_deluxe <milagalko1@gmail.com>
2022-10-10 19:54:41 +0300

    feat: cell class

---
+++ Pacman/Pacman/Classes/Cell.cs
@@ -0,0 +1,15 @@
+namespace Pacman.Classes;
+
+public class Cell
+{
+    public int GetScore()
+    {
+        return 1;
+    }
+
+    // TODO: Get adjacent cells?
+    public Cell[] GetAdjCells()
+    {
+        return new Cell();
+    }
+}
