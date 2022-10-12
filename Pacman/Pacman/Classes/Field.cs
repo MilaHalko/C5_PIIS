7ca00949195703188d6d2d9970088832b7e87498
milka_deluxe <milagalko1@gmail.com>
2022-10-13 00:33:19 +0300

    fix: new attributes

--- Pacman/Pacman/Classes/Field.cs
+++ Pacman/Pacman/Classes/Field.cs
@@ -2,23 +2,23 @@ namespace Pacman.Classes;
 public class Field
 {
     private int[,] _maze;
-    private int _height = 5;
-    private int _width = 7;
-    private int[] _pacman = new int[] { 1, 1 };
-    private int[] _enemy = new int[] { 0, 0 };
-    private int[] _destPoint = new int[] { 5, 3 };
+    public int Height { get; } = 5;
+    public int Width { get; } = 7;
+    
+    public int this[int x, int y] => _maze[x, y];
+    public int this[Cell cell] => _maze[cell.X, cell.Y];
 
     private Field(int[,] maze)
     {
         this._maze = maze;
     }
 
-    public static Field GetDefaultField() => new(new[,]
+    public static Field GetDefaultField() => new Field(new[,]
     {
-        { 6, 1, 1, 1, 1, 1, 1 },
-        { 1, 3, 1, 0, 1, 1, 1 },
+        { 1, 1, 1, 1, 1, 1, 1 },
+        { 1, 1, 1, 0, 1, 1, 1 },
         { 0, 0, 1, 0, 1, 0, 0 },
-        { 1, 1, 1, 0, 1, 9, 1 },
+        { 1, 1, 1, 0, 1, 1, 1 },
         { 1, 1, 1, 1, 1, 1, 1 },
     });
 
@@ -31,48 +31,4 @@ public class Field
     🟪▪️▪️▪️▪️▪️▪️▪️🟪
     🟪🟪🟪🟪🟪🟪🟪🟪🟪
      */
-
-    public bool EnemyWon()
-    {
-        return _pacman[0] == _enemy[0] && _pacman[1] == _enemy[1];
-    }
-    public void PrintField()
-    {
-        string[,] stringMaze = new string[_height, _width];
-        for (int i = 0; i < _height; i++)
-        {
-            for (int j = 0; j < _width; j++)
-            {
-                switch (_maze[i, j])
-                {
-                    case 0:
-                        stringMaze[i, j] = "🟪";
-                        break;
-                    case 1:
-                        stringMaze[i, j] = "⬛️";
-                        break;
-                    case 3:
-                        stringMaze[i, j] = "😇";
-                        break;
-                    case 6:
-                        stringMaze[i, j] = "👿";
-                        break;
-                    case 9:
-                        stringMaze[i, j] = "🟥";
-                        break;
-                }
-            }
-        }
-        Console.WriteLine("🟪🟪🟪🟪🟪🟪🟪🟪🟪");
-        for (int i = 0; i < _height; i++)
-        {
-            Console.Write("🟪");
-            for (int j = 0; j < _width; j++)
-            {
-                Console.Write(stringMaze[i, j]);
-            }
-            Console.WriteLine("🟪");
-        }
-        Console.WriteLine("🟪🟪🟪🟪🟪🟪🟪🟪🟪");
-    }
 };
