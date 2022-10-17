ae847269c7d23e4cb96f7bf8af6b238da65d4c24
vakor <vakor.dev@gmail.com>
2022-10-17 21:26:38 +0300

    Vakor's commit

---
+++ Pacman/Pacman/PathAlgoClasses/PathNode.cs
@@ -0,0 +1,6 @@
+namespace Pacman.PathAlgoClasses;
+
+public class PathNode
+{
+    
+}
--- Pacman/Pacman/PathAlgos/AStar.cs
+++ Pacman/Pacman/PathAlgos/AStar.cs
@@ -3,21 +3,43 @@ namespace Pacman.PathAlgos;
 
 public class AStar : IPathSearch
 {
-    private  List<State> _openNodes = new List<Cell>();
+    private  PriorityQueue<State, int> _openNodes = new List<Cell>();
     private List<State> _closedNodes = new List<Cell>();
-    
     
     public int FindPath(State state)
     {
-        _openNodes.Add(state);
+        _openNodes.Add(state, GetF());
         _openNodes.Add(state.GetAdjacents());
-        
-        // TODO return valid please
+
+        while (_openNodes.Count != 0)
+        {
+            
+        }
         return 1;
     }
-
+    
     private Cell[] ReconstructPath()
     {
         
+    }
+}
+
+class PathNode
+{
+    public PathNode parent { get; }
+    public Cell cell { get; }
+    public int g { get; }
+    public int h { get; }
+
+    public PathNode(Cell cell, PathNode parent, int distance, int heuristic = 1)
+    {
+        this.cell = cell;
+        this.parent = parent;
+        g = distance;
+        h = heuristic;
+    }
+    public int GetF()
+    {
+        return g + h;
     }
 }
